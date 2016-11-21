3258e02a91d7412ceb9033d7b9765d3132a317f7
Jordi Dosne <jordi@dosne.net>
2016-11-21 21:36:19 +0100

    BUG-987

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -16,20 +16,29 @@ const initialState = {
 
 function gameReducer (state = initialState, action) {
     switch (action.type) {
-        case SELECT_EDGE:
+        case SELECT_EDGE: {
+            const selectedPath = getPathCoordinatesFromCellEdge(action.colIndex, action.rowIndex, action.orientation)
+            const selectedPathAvailable = state.availablePaths.find(path => {
+                return path.row === selectedPath.row && path.column === selectedPath.column
+            })
+
             return {
                 ...state,
-                selectedPath: getPathCoordinatesFromCellEdge(action.colIndex, action.rowIndex, action.orientation)
+                selectedPath: selectedPathAvailable
             }
-        case VALIDATE_PATH:
-            state.availablePaths.find(function () {
+        }
+        case VALIDATE_PATH: {
+            const availablePaths = state.availablePaths.filter(function (path) {
+                return !(path.row === state.selectedPath.row && path.column === state.selectedPath.column)
             })
 
             return {
                 ...state,
                 validatedPaths: state.validatedPaths.concat(state.selectedPath),
+                availablePaths,
                 selectedPath: null
             }
+        }
         case UNVALIDATE_PATH:
             return {
                 ...state,
